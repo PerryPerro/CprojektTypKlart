e6259383dbe4bd523d2c5b57f39a355daef36e0d
PerryPerro <Andreas.persson09@gmail.com>
2017-10-27 15:52:47 +0200

    Add files via upload

---
+++ Logic/Class2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Validator
+    {
+        public class validera
+        {
+            public validera()
+            {
+            }
+            public static bool emptyTextbox(string text)
+            {
+                if (string.IsNullOrEmpty(text) || text == "")
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            public void realURL()
+            {
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# CprojektTypKlart
-lul
+# Cprojekt-4
